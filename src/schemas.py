2a388e8d84e5157903a67ca213b430ad492d876f
hamurda <damlahmrc@gmail.com>
2025-07-09 18:03:43 +1000

    Implements summariser and cacheing

---
+++ src/schemas.py
@@ -0,0 +1,36 @@
+from typing import TypedDict, List, Literal
+
+
+class TopicDict(TypedDict):
+    label: str
+    score: float
+
+class TickerSentimentDict(TypedDict):
+    ticker: str
+    label: str
+    confidence: float
+    sentence: str
+
+class ArticleDict(TypedDict):
+    title: str
+    description: str
+    content: str
+    published_at: str
+    url: str
+    source: str
+    overall_sentiment_label: str
+    overall_sentiment_score: float
+    topics: List[TopicDict]
+    ticker_sentiment: List[TickerSentimentDict]
+
+
+class SummaryDict(TypedDict):
+    title: str
+    summary: str
+    source: str
+    published_at: str
+    url: str
+    sentiment: str
+    sentiment_score: float
+    topics: List[TopicDict]
+    ticker_sentiment: List[TickerSentimentDict]
---
+++ src/summarizer.py
@@ -0,0 +1,124 @@
+import os
+from src.schemas import ArticleDict, SummaryDict
+from src.utils.cache import load_from_cache, save_to_cache
+import hashlib
+
+import torch
+import ollama
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+_model_id = "us4/fin-llama3.1-8b"
+_tokenizer = AutoTokenizer.from_pretrained(_model_id)
+
+_bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16
+)
+
+_model = AutoModelForCausalLM.from_pretrained(
+    _model_id,
+    quantization_config=_bnb_config,
+    device_map="auto"
+)
+
+class FinNewsSummarizer:
+    DEFAULT_OLLAMA_MODEL = "llama3.2"
+    SYSTEM_PROMPT = """You are a financial news analyst assistant.
+    Given a financial news article, your task is to extract a structured summary with the following sections:
+    -Market Summary  
+    -Valuation Metrics  
+    -Macro & FX  
+    -Key Drivers  
+    -Tickers  
+    -Sentiment
+    Be concise, accurate, and avoid repeating the entire article. Reply in markdown format.
+    Do not add information. Summarise only the written information in the given article.
+    """
+
+    def __init__(self, model=_model, tokenizer=_tokenizer, cache_dir="data/summaries", use_local_model=True):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.cache_dir = cache_dir
+        self.use_local_model = use_local_model
+
+    def summarize(self, article: ArticleDict) -> str:
+        article_id = self._hash(article["url"])
+        cache_path = os.path.join(self.cache_dir, f"{article_id}.json")
+        if os.path.exists(cache_path):
+            return load_from_cache(cache_path)
+        
+        if self.use_local_model:
+            summary = self._summarize_llama32(article)
+        else:
+            summary = self._summarize_fin_llama31(article)
+
+        summarised: SummaryDict = {
+                "title": article.get("title", ""),
+                "summary": summary,
+                "description": article.get("description", ""),
+                "published_at": article.get("publishedAt", ""),
+                "url": article.get("url", ""),
+                "source": article.get("source", {}).get("name", ""),
+                "sentiment": article.get("overall_sentiment_label", ""),
+                "sentiment_score": article.get("overall_sentiment_score", 0),
+                "topics": article.get("topics", []),
+                "ticker_sentiment": article.get("ticker_sentiment", []),
+            }
+
+        save_to_cache(summarised, cache_path)
+        return summarised
+    
+    def summarize_all(self, article_dir: str = "data/cache"):
+        for file in os.listdir(article_dir):
+            if not file.endswith(".json"):
+                continue
+            article = load_from_cache(os.path.join(article_dir, file))
+            self.summarize(article)
+
+    def _summarize_llama32(self, article):
+        """
+        Placeholder summarizing method to use llama3.2 locally. Otherwise GPU needed.
+        """
+        messages = [
+            {"role": "system", "content":self.SYSTEM_PROMPT},
+            {"role": "user", "content": self._build_prompt(article)}
+        ]
+
+        response = ollama.chat(model=self.DEFAULT_OLLAMA_MODEL, messages=messages, 
+                               stream=False,
+                               options= {
+                                    "temperature": 0.7
+                                })
+    
+        return response['message']['content']
+    
+    def _summarize_fin_llama31(self, article):
+        """
+        Using "us4/fin-llama3.1-8b" to summarize finance news.
+        """
+        prompt = self._build_prompt(article)
+        inputs = self.tokenizer(prompt, return_tensors="pt").to("cuda")
+
+        outputs = self.model.generate(
+            **inputs,
+            max_new_tokens=1000,
+            temperature=0.7,
+        )
+
+        response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return response[len(prompt):].strip()
+    
+    def _build_prompt(self, article: ArticleDict) -> str:
+        return (
+            f"<|system|>\n{self.SYSTEM_PROMPT}\n"
+            f"<|user|>\n"
+            f"### News Title:\n{article['title']}\n\n"
+            f"### Article:\n{article['content']}\n\n"
+            f"### The summary:\n"
+            f"<|assistant|>\n"
+        )
+
+    def _hash(self, text: str) -> str:
+        return hashlib.sha1(text.encode()).hexdigest()
