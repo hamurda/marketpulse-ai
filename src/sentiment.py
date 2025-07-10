8bfce33b9d72a1372ed8394ece29f9e3050aad6f
hamurda <damlahmrc@gmail.com>
2025-07-10 15:07:22 +1000

    adding streamlit cache resource to sentiment pipeline

--- src/sentiment.py
+++ src/sentiment.py
@@ -1,11 +1,16 @@
+import streamlit as st
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
-_model_name = "ProsusAI/finbert"
-_tokenizer = AutoTokenizer.from_pretrained(_model_name)
-_model = AutoModelForSequenceClassification.from_pretrained(_model_name)
 
-_sentiment_pipeline = pipeline("sentiment-analysis", model=_model, tokenizer=_tokenizer)
+@st.cache_resource
+def load_sentiment_pipeline():
+    model_name = "ProsusAI/finbert"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
+    return pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+_sentiment_pipeline = load_sentiment_pipeline()
 
 def classify_sentiment(text: str) -> tuple[str, float]:
     result = _sentiment_pipeline(text[:512])[0]  # truncate to avoid max token errors
